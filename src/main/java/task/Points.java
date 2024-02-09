5fa9afbc6f666653f4f5646f71cfacdd7b92e5b6
Vladislav20V23 <vlad.zhuqan@yandex.ru>
2024-02-09 12:51:04 +0300

    go

--- src/main/java/task/Points.java
+++ src/main/java/task/Points.java
@@ -12,11 +12,15 @@ public class Points {
     Point2D point2D;
 
     public Points() {
-        Point2D p1 = new Point2D.Double (2, 3);
-        Point2D p2 = new Point2D.Double(8, 4.3);
-        point2Ds = new Point2D.Double[]{(Point2D.Double) p1, (Point2D.Double) p2};
-        point2D = new Point2D.Double(6, 6);
+        Point2D p1 = new Point2D.Double (0, 0);
+        Point2D p2 = new Point2D.Double(1, 6);
+        Point2D p3 = new Point2D.Double(6, 6);
+        point2Ds = new Point2D.Double[]{(Point2D.Double) p1, (Point2D.Double) p2, (Point2D.Double) p3};
+        point2D = new Point2D.Double(0, 0);
     }
+
+
+
 
     @Override
     public String toString() {
